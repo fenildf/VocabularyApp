38d222f7b09bd2a867f1a34ba2a6fb00b008b230
zhaoyangcui <cuizhaoyanghrb@163.com>
2016-11-21 19:15:05 +0800

    安卓实验项目 单词本

---
+++ VocabularyApp/src/main/java/app/company/com/a20_sqlite/data/Word.java
@@ -0,0 +1,31 @@
+package app.company.com.a20_sqlite.data;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2016/10/4.
+ */
+public class Word implements Serializable{
+    private String word = "";
+    private String meaning = "";
+    private String sample = "";
+
+    public Word(String word, String meaning, String sample){
+        this.word = word;
+        this.meaning = meaning;
+        this.sample = sample;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public String getSample() {
+        return sample;
+    }
+
+}
